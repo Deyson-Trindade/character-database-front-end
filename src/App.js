21ac346ae9918c3257001685a9dc9222755fc42b
Deyson <trindadedeyson1@gmail.com>
2021-07-05 21:50:09 -0300

    NavBar criado e iniciando Formulário

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Hearders from './components/Headers'
+import FormCharacter from './components/FormCharacter'
+
+
+function App() {
+  return (
+    <div >
+      <Hearders />
+      <FormCharacter />
+    </div>
+  );
+}
+
+export default App;
